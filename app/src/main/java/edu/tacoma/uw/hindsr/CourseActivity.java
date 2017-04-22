8f42e87ca1ab35d81e92281f8c9bdac405b98464
rhinds1004 <rhinds1004@gmail.com>
2017-04-22 14:41:07 -0700

    Initial commit

---
+++ app/src/main/java/edu/tacoma/uw/hindsr/CourseActivity.java
@@ -0,0 +1,13 @@
+package edu.tacoma.uw.hindsr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+    }
+}
